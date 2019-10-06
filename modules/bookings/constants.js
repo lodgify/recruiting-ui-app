bc71e7267e37fef47307e0168b55994bae8335bd
ivan-herrero <ivanhnun@gmail.com>
2019-10-06 11:32:24 +0200

    Add ListItem component

--- modules/bookings/constants.js
+++ modules/bookings/constants.js
@@ -1,1 +1,31 @@
+import { object, oneOf, bool, string, number } from 'prop-types';
+
 export const MODULE_NAME = 'bookings';
+
+export const BookingStatus = Object.freeze({
+  Booked: 'Booked',
+  Declined: 'Declined',
+  Unavailable: 'Unavailable',
+  Tentative: 'Tentative',
+  Open: 'Open',
+});
+
+export const Booking = {
+  id: string.isRequired,
+  dateCreated: object.isRequired,
+  dateArrival: object.isRequired,
+  dateDeparture: object.isRequired,
+  status: oneOf([
+    BookingStatus.Booked,
+    BookingStatus.Declined,
+    BookingStatus.Unavailable,
+    BookingStatus.Tentative,
+    BookingStatus.Open,
+  ]).isRequired,
+  replied: bool.isRequired,
+  guestName: string.isRequired,
+  propertyName: string.isRequired,
+  people: number.isRequired,
+  currencyCode: string.isRequired,
+  amountPaid: number.isRequired,
+};
