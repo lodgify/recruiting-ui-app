89f50cbc3fa15f1baf403f8319da3a02cf444de9
c.ashurbeyli <c.ashurbeyli@gmail.com>
2019-10-28 16:56:46 +0100

    feat: initial commit to make wrapper for sections

--- modules/bookings/components/BookingsPage/component.js
+++ modules/bookings/components/BookingsPage/component.js
@@ -22,11 +22,17 @@ export class Component extends React.PureComponent {
     if (this.props.hasFailed || !this.props.data) return 'Failed :(';
 
     return (
-      <>
-        <h1 className={styles.Title}>Welcome!</h1>
-        <CalendarLogo />
-        {this.props.data.map(booking => booking.guestName)}
-      </>
+      <div className={styles.GridContainer}>
+        <div className={`${styles.GridItem} ${styles.ListSection}`}>
+          <ul>
+            {this.props.data.map(booking => <li key={booking.guestName}>{booking.guestName}</li>)}
+          </ul>
+        </div>
+        <div className={`${styles.GridItem} ${styles.MainSection}`}>
+          <h1 className={styles.Title}>Welcome!</h1>
+          <CalendarLogo />
+        </div>
+      </div>
     );
   }
 }
