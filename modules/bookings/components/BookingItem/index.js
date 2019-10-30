69cf8062a4b0936eb1650279cac2d5aea6910eee
Erkman Original <harren@Erkmans-MBP.home>
2019-10-30 01:47:29 +0100

    Add BookingItem component

---
+++ modules/bookings/components/BookingItem/index.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { object } from 'prop-types';
+import { format, differenceInCalendarDays } from 'date-fns';
+
+import styles from './styles.css';
+import ReplyIcon from '../../../../static/reply.svg';
+
+const BookingItem = ({ booking }) => {
+  const bookingStatus = {
+    Booked: 'Green',
+    Unavailable: 'Gray',
+    Tentative: 'Orange',
+    Open: 'Orange',
+    Declined: 'Red',
+  };
+
+  return (
+    <div className={styles.BookingItem}>
+      <div className={styles.Row}>
+        <div className={styles.GuestName}>
+          <div
+            className={`${styles.StatusIndicator} ${
+              styles[bookingStatus[booking.status]]
+            }`}
+          />
+          {booking.guestName}
+        </div>
+        <div className={styles.CreatedDate}>
+          {format(booking.dateCreated, 'dd/MM/yyyy')}
+        </div>
+      </div>
+
+      <div className={styles.Row}>
+        <div>
+          {booking.replied ? <ReplyIcon width="1rem" height="1rem" /> : null}
+          {booking.propertyName}
+        </div>
+      </div>
+
+      <div className={styles.Row}>
+        <div>
+          <span>{format(booking.dateArrival, 'MMM dd yyyy')}, </span>
+          <span>
+            {differenceInCalendarDays(booking.dateDeparture, booking.dateArrival)} nights
+          </span>
+          <span> {booking.people} people</span>
+        </div>
+        <div>
+          {booking.amountPaid} {booking.currencyCode}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+BookingItem.propTypes = {
+  booking: object,
+};
+
+export default BookingItem;
