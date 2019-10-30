01930c7f7e81bc0d6d572cabc4191e9e952e73d5
Erkman Original <harren@Erkmans-MBP.home>
2019-10-30 01:48:26 +0100

    Use BookingList component

--- modules/bookings/components/BookingsPage/BookingsList.js
+++ modules/bookings/components/BookingsPage/BookingsList.js
@@ -1,26 +1,28 @@
 import React, { useState } from 'react';
-import {arrayOf, shape, string } from 'prop-types';
+import { arrayOf, shape, string } from 'prop-types';
+
+import BookingItem from '../BookingItem'
+import styles from './styles.css';
 
 const BookingsList = props => {
-
   const [filter, setFilter] = useState('');
 
-  const filteredBookings = props.data.filter(booking => booking.guestName.toLowerCase().includes(filter))
+  const filteredBookings = props.data.filter(booking =>
+    booking.guestName.toLowerCase().includes(filter.toLowerCase()),
+  );
 
   return (
-    <>
-        <input type="text" value={filter} onChange={e => setFilter(e.target.value)}></input>
-        <p>{filter}</p>
-        {filteredBookings.map(booking => {
-        return (
-            <div key={booking.id}>
-                <p>{booking.guestName}</p>
-            </div>
-        )
-        })}
-    </>
-    );
-}
+    <div className={styles.BookingsList}>
+      <input
+        placeholder="Search"
+        type="text"
+        value={filter}
+        onChange={e => setFilter(e.target.value)}
+      />
+      {filteredBookings.map(booking => <BookingItem key={booking.id} booking={booking}></BookingItem>)}
+    </div>
+  );
+};
 
 BookingsList.propTypes = {
   data: arrayOf(shape({ guestName: string })),
