d62e4bfb2bb61394a1293838ebf05f83122e5f9e
Adrian Zailic <adrianzailic@yahoo.com>
2019-10-31 08:14:26 +0200

    Scroll bar style

--- modules/bookings/components/BookingList/index.js
+++ modules/bookings/components/BookingList/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import BookingItem from '../BookingItem';
+import './styles.css';
 
 export default function BookingList({ bookings }) {
   return (
